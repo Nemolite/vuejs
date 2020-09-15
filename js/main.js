22d1d1ab1cdbd85e8f154274039f59c8679493d6
Nemolite <g25092011@mail.ru>
2020-09-15 20:27:14 +0300

    array

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let app = new Vue({
+	el: '#app',
+	data: {
+		message1: 'hello1',
+		message2: 'hello2',
+		name: 'Вася',
+	    surname: 'Иванов',
+	},
+});
+
+
+let app2 = new Vue({
+	el: '#app2',
+	data: {
+		var1: 1,
+		var2: 2,
+		num: 5,
+	},
+});
+
+let app3 = new Vue({
+	el: '#app3',
+	data: {
+		arr: [1, 2, 3],
+	},
+});
