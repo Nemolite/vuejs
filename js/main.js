78db5ee033da4bec6a460239495bc2b3faf9dc1f
Nemolite <g25092011@mail.ru>
2020-09-16 07:14:19 +0300

    array+array

--- js/main.js
+++ js/main.js
@@ -63,4 +63,103 @@ let app8 = new Vue({
 	},
 });
 
+let app9 = new Vue({
+	el: '#app9',
+	data: {
+		array: ['user1', 'user2', 'user3']
+	},
+});
 
+let app10 = new Vue({
+	el: '#app10',
+	data: {
+		users: {user1: '300$', user2: '400$', user3: '500$'},
+	},
+});
+
+
+let app11 = new Vue({
+	el: '#app11',
+	data: {
+		users: {user1: '300$', user2: '400$', user3: '500$'},
+	},
+});
+
+
+let app12 = new Vue({
+	el: '#app12',
+	data: {
+		users: {user1: '300$', user2: '400$', user3: '500$'},
+	},
+});
+
+
+
+let app13 = new Vue({
+	el: '#app13',
+	data: {
+		arr13: 
+
+		{
+	employee1: '100$',
+	employee2: '200$',
+	employee3: '300$',
+} //arr13
+	}, //data
+});
+
+
+let app14 = new Vue({
+	el: '#app14',
+	data: {
+		arr14: 
+
+		{
+	employee1: '100$',
+	employee2: '200$',
+	employee3: '300$',
+} //arr14
+	}, //data
+});
+
+
+let app15 = new Vue({
+	el: '#app15',
+	data: {
+		arr15: 
+
+		{
+	employee1: '100$',
+	employee2: '200$',
+	employee3: '300$',
+} //arr15
+	}, //data
+});
+
+let app16 = new Vue({
+	el: '#app16',
+	data: {
+		arr16: 
+
+		{
+	employee1: '100$',
+	employee2: '200$',
+	employee3: '300$',
+} //arr16
+	}, //data
+});
+
+
+// Многомерные массивы и циклы
+
+let app17 = new Vue({
+	el: '#app17',
+	data: {
+		// Элемент массива = массив
+		users: [
+			{name: 'user1', salary: '300$'},
+			{name: 'user2', salary: '400$'},
+			{name: 'user3', salary: '500$'},
+		],
+	},
+});
